e58dcb118a91043318e058d9007ce48c25ecef2e
Andrew <drew29799@gmail.com>
2024-05-12 19:37:42 +0300

    Add: async app controller functions

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -8,7 +8,7 @@ class AppController {
    * @param {request} req API Request object
    * @param {response} res API Response object
    */
-  static getStatus(req, res) {
+  static async getStatus(req, res) {
     const dbStatus = dbClient.isAlive();
     const redisStatus = redisClient.isAlive();
     res.status(200).json({ redis: redisStatus, db: dbStatus });
@@ -19,9 +19,9 @@ class AppController {
    * @param {request} req API request object
    * @param {response} res API response object
    */
-  static getStats(req, res) {
-    const numUsers = dbClient.nbUsers();
-    const numFiles = dbClient.nbFiles();
+  static async getStats(req, res) {
+    const numUsers = await dbClient.nbUsers();
+    const numFiles = await dbClient.nbFiles();
     res.status(200).json({ users: numUsers, files: numFiles });
   }
 }
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import routes from './routes';
 
-const PORT = process.env.PORT ? process.env.PORT : 5000;
+const { PORT = 5000 } = process.env.PORT;
 const app = express();
 
+app.use(express.json());
 app.use('/', routes);
 
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
